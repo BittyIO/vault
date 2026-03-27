5d600019848de50e06ca7ee019283df9982dbd8a
AndyChanWeb3 <piratecodeweb3@gmail.com>
2026-03-27 13:11:09 +1100

    withdraw eth send back to vault from lido

--- test/fork/LidoProvider.fork.t.sol
+++ test/fork/LidoProvider.fork.t.sol
@@ -7,8 +7,43 @@ import {LidoV2Provider} from "../../src/providers/LidoV2Provider.sol";
 import {mainnet} from "../../script/addresses.sol";
 import {IStETH, IUnstETH} from "../../src/libs/lido/v2/Lido.sol";
 import {WETH} from "solmate/tokens/WETH.sol";
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {Address} from "openzeppelin-contracts/contracts/utils/Address.sol";
 import {WETHBalanceNotEnough} from "../../src/interfaces/IVault.sol";
+
+/// @dev Simulates Lido's unstETH: marks all requests as finalized and sends ETH on claimWithdrawal
+contract MockUnstETHSendsEth {
+    uint256 public ethPerClaim;
+
+    constructor(uint256 ethPerClaim_) payable {
+        ethPerClaim = ethPerClaim_;
+    }
+
+    function getWithdrawalStatus(uint256[] calldata requestIds)
+        external
+        view
+        returns (IUnstETH.WithdrawalRequestStatus[] memory statuses)
+    {
+        statuses = new IUnstETH.WithdrawalRequestStatus[](requestIds.length);
+        for (uint256 i = 0; i < requestIds.length; i++) {
+            statuses[i] = IUnstETH.WithdrawalRequestStatus({
+                amountOfStETH: ethPerClaim,
+                amountOfShares: 0,
+                owner: address(this),
+                timestamp: block.timestamp,
+                isFinalized: true,
+                isClaimed: false
+            });
+        }
+    }
+
+    function claimWithdrawal(uint256) external {
+        (bool success,) = msg.sender.call{value: ethPerClaim}("");
+        require(success, "ETH transfer failed");
+    }
+
+    receive() external payable {}
+}
 
 contract TestLidoProviderFork is Test {
     using Address for address;
@@ -56,6 +91,52 @@ contract TestLidoProviderFork is Test {
         weth.approve(address(lidoProvider), stakeAmount);
         vm.expectRevert(WETHBalanceNotEnough.selector);
         lidoProvider.stake(stakeAmount + 1 ether);
+    }
+
+    function test_Claim_ReturnWETHToVault() public {
+        uint256 claimAmount = 1 ether;
+        MockUnstETHSendsEth mockUnstETH = new MockUnstETHSendsEth{value: claimAmount}(claimAmount);
+        LidoV2Provider provider = new LidoV2Provider(mainnet.STETH, address(mockUnstETH), mainnet.WETH);
+        provider.initialize(address(this));
+
+        uint256 wethBefore = weth.balanceOf(address(this));
+
+        uint256[] memory requestIds = new uint256[](1);
+        requestIds[0] = 1;
+        provider.claim(requestIds);
+
+        assertEq(address(provider).balance, 0, "provider should have 0 ETH after claim");
+        assertEq(IERC20(mainnet.WETH).balanceOf(address(provider)), 0, "provider should have 0 WETH after claim");
+        assertEq(
+            IERC20(mainnet.WETH).balanceOf(address(this)),
+            wethBefore + claimAmount,
+            "WETH returned to vault after claim"
+        );
+    }
+
+    function test_Claim_MultipleRequests_ReturnWETHToVault() public {
+        uint256 ethPerClaim = 0.5 ether;
+        uint256 numRequests = 3;
+        uint256 totalEth = ethPerClaim * numRequests;
+        MockUnstETHSendsEth mockUnstETH = new MockUnstETHSendsEth{value: totalEth}(ethPerClaim);
+        LidoV2Provider provider = new LidoV2Provider(mainnet.STETH, address(mockUnstETH), mainnet.WETH);
+        provider.initialize(address(this));
+
+        uint256 wethBefore = weth.balanceOf(address(this));
+
+        uint256[] memory requestIds = new uint256[](numRequests);
+        for (uint256 i = 0; i < numRequests; i++) {
+            requestIds[i] = i + 1;
+        }
+        provider.claim(requestIds);
+
+        assertEq(address(provider).balance, 0, "provider should have 0 ETH after claim");
+        assertEq(IERC20(mainnet.WETH).balanceOf(address(provider)), 0, "provider should have 0 WETH after claim");
+        assertEq(
+            IERC20(mainnet.WETH).balanceOf(address(this)),
+            wethBefore + totalEth,
+            "all WETH returned to vault after claim"
+        );
     }
 
     function test_Claim_emptyUnstakeRequests_doesNotRevert() public {
