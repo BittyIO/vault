b4aa147be238f9fb4d95a2807e20cef5690a6d7a
AndyChanWeb3 <piratecodeweb3@gmail.com>
2026-05-18 16:02:14 +1000

    add deploy script for factory

--- script/addresses.sol
+++ script/addresses.sol
@@ -4,11 +4,9 @@ pragma solidity ^0.8.34;
 library mainnet {
     address public constant WHITE_LIST = 0x00000000E30003A97Fc8A8A3c293b5f01FB4f525;
     address public constant SUBSCRIPTION = 0x0000000099e7d10b6169A893C037a4c4e237B853;
-    address public constant VAULT_IMPL = 0x8D1a105Bbd2818A8D5915f726259aFE0ff0680b6;
 }
 
 library sepolia {
     address public constant WHITE_LIST = 0x00000000E30003A97Fc8A8A3c293b5f01FB4f525;
     address public constant SUBSCRIPTION = 0x0000000099e7d10b6169A893C037a4c4e237B853;
-    address public constant VAULT_IMPL = 0x8D1a105Bbd2818A8D5915f726259aFE0ff0680b6;
 }
